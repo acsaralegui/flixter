f7902d8ea68531fa6f90dfd012ffe71da9175039
acsaralegui <aaron.saralegui@gmail.com>
2019-06-22 14:26:01 +0000

    course validation

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,16 +1,20 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
 
-  def new
+   def new
     @course = Course.new
   end
 
   def create
     @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-   def show
+  def show
     @course = Course.find(params[:id])
   end
 
@@ -19,4 +23,9 @@ class Instructor::CoursesController < ApplicationController
   def course_params
     params.require(:course).permit(:title, :description, :cost)
   end
-end
+end 
+ 
+ 
+ 
+ 
+
