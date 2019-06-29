851a4c80c1f46f1ae5614c4acbd5d176bbed4f85
acsaralegui <aaron.saralegui@gmail.com>
2019-06-29 16:25:44 +0000

    fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIA5TAQSAQPS6WGWXFW"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["saralegui-flixter"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
