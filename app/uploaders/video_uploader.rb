075e8507e26c5a36e6252873667da738282f1237
acsaralegui <aaron.saralegui@gmail.com>
2019-07-01 15:07:26 +0000

    flixing video embedding

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -3,16 +3,15 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-   if Rails.env.production?
-   storage :aws
-  else
-    storage :file
-  end
-  
+  # Choose what kind of storage to use for this uploader:
+  # storage :file
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
